e0952cd6c934c7b5c5a07c795cbdec93443c8bf5
Gkenzhebekova96 <Brazil52@>
2023-04-29 21:16:18 +0400

    we added new line

--- mySecondFile.java
+++ mySecondFile.java
@@ -2,6 +2,8 @@ public class mySecondFile {
     public static void main(String[] args) {
         System.out.println(" MAy test");
         System.out.println(" My new branch");
+        String day = " Saturday";
+        System.out.println(day);
 
     }
 }
